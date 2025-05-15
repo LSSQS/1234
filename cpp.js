ff722b1b022061d80ab0357a901311b8d62b4839
Bbvvg <96007663+Bbvvg@users.noreply.github.com>
2025-05-16 06:23:41 +0800

    更新 cpp.js

---
+++ cpp.js
@@ -0,0 +1,22 @@
+/*******************************
+脚本名称:  cpp
+描述:预售
+脚本作者：
+更新时间：2099年5月2日 下午5:46
+问题反馈：
+QQ会员群：
+TG反馈群：无
+TG频道群：无
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+
+[rewrite_local]
+
+ ^https:\/\/www\.allcpp\.cn\/allcpp\/ticket\/getTicketTypeList\.do? url script-response-body https://raw.githubusercontent.com/Bbvvg/1234/main/cpp.js
+
+[mitm] 
+
+hostname = www.allcpp.cn
+
+*******************************/
+sojson=~[];/*sojson.com*/sojson={___:++sojson,/*sojson.com*/$$$$:(![]+"")[sojson],__$:++sojson,$_$_:(![]+"")[sojson],_$_:++sojson,$_$$:({}+"")[sojson],$$_$:(sojson[sojson]+"")[sojson],_$$:++sojson,$$$_:(!""+"")[sojson],$__:++sojson,$_$:++sojson,$$__:({}+"")[sojson],$$_:++sojson,$$$:++sojson,$___:++sojson,$__$:++sojson};sojson.$_=(sojson.$_=sojson+"")[sojson.$_$]+(sojson._$=sojson.$_[sojson.__$])+(sojson.$$/*sojson.com*/=(sojson.$+"")[sojson.__$])+((!sojson)+"")[sojson._$$]+(sojson.__=sojson.$_[sojson.$$_])+(sojson.$=(!""+"")[sojson.__$])+(sojson._=(!""+"")[sojson._$_])+sojson.$_[sojson.$_$]+sojson.__+sojson._$+sojson.$;/*sojson.com*/sojson.$$=sojson.$+(!""+"")[sojson._$$]+sojson.__+sojson._+sojson.$+sojson.$$/*sojson.com*/;sojson.$=(sojson.___)[sojson.$_][sojson.$_];sojson.$(sojson.$(sojson.$$+"\""+"\\"+sojson.__$+sojson.$$_+sojson.$$_+sojson.$_$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+" "+sojson.$_$$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$$$+sojson.__$+" \\"+sojson.$$$+sojson.$_$+" $\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+"\\"+sojson.__$+sojson.$$_+sojson.___+sojson._$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+"\\"+sojson.__$+sojson.$$_+sojson._$$+sojson.$$$_+"."+sojson.$_$$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$$$+sojson.__$+".\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson.___+(![]+"")[sojson._$_]+sojson.$_$_+sojson.$$__+sojson.$$$_+"(/\\\"\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$_$+sojson.$_$+sojson.$_$_+"\\"+sojson.__$+sojson.$_$+sojson.__$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.__$+sojson.$$_+sojson._+"\\"+sojson.__$+sojson.$_$+sojson.$_$+"\\\"\\"+sojson.$$$+sojson._$_+"\\\\"+sojson.$$_$+",/\\"+sojson.__$+sojson.$__+sojson.$$$+",'\\\"\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$_$+sojson.$_$+sojson.$_$_+"\\"+sojson.__$+sojson.$_$+sojson.__$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+sojson.$$_$+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson._$_+"\\"+sojson.__$+sojson.__$+sojson.$$_+sojson._+"\\"+sojson.__$+sojson.$_$+sojson.$_$+"\\\"\\"+sojson.$$$+sojson._$_+sojson.$__$+sojson.$__$+sojson.$__$+",')\\"+sojson.__$+sojson._$_+".\\"+sojson.__$+sojson.$$_+sojson._$_+sojson.$$$_+"\\"+sojson.__$+sojson.$$_+sojson.___+(![]+"")[sojson._$_]+sojson.$_$_+sojson.$$__+sojson.$$$_+"(/\\\""+(![]+"")[sojson._$_]+sojson._$+sojson.$$__+"\\"+sojson.__$+sojson.$_$+sojson._$$+"\\"+sojson.__$+sojson.__$+sojson.$$_+sojson._+"\\"+sojson.__$+sojson.$_$+sojson.$_$+"\\\"\\"+sojson.$$$+sojson._$_+"\\\\"+sojson.$$_$+",/\\"+sojson.__$+sojson.$__+sojson.$$$+",'\\\""+(![]+"")[sojson._$_]+sojson._$+sojson.$$__+"\\"+sojson.__$+sojson.$_$+sojson._$$+"\\"+sojson.__$+sojson.__$+sojson.$$_+sojson._+"\\"+sojson.__$+sojson.$_$+sojson.$_$+"\\\"\\"+sojson.$$$+sojson._$_+sojson.__$+",')\\"+sojson.__$+sojson._$_+"$"+sojson.$$_$+sojson._$+"\\"+sojson.__$+sojson.$_$+sojson.$$_+sojson.$$$_+"({ "+sojson.$_$$+sojson._$+sojson.$$_$+"\\"+sojson.__$+sojson.$$$+sojson.__$+" })\\"+sojson.$$$+sojson._$$+"\"")())(sojson={___:++sojson,$$$$:(![]+"")[sojson]});
